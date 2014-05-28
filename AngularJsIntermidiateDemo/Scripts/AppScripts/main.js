e41d2ddd3b825bd1b90239f0a841ca51e85fcf25
fiyazbinhasan <fiyazhasan@live.com>
2014-05-28 22:36:37 +0600

    some

---
+++ AngularJsIntermidiateDemo/Scripts/AppScripts/main.js
@@ -0,0 +1,16 @@
+﻿var main = angular.module('main', ['ngRoute']);
+
+main.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/DOT', {
+        templateUrl: "Partials/DOT.html",
+        controller: "DOTController"
+    }).when('/Promises', {
+        templateUrl: "Partials/Promises.html",
+        controller: "PromisesController"
+    }).otherwise({ redirectTo: '/DOT' });
+}]);
+
+main.controller('DOTController', function($scope) {
+});
+main.controller('PromisesController', function ($scope) {
+});
